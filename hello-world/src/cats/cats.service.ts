62c5b960b4f82fd5c2753fa2a5329c8eaf869d79
KrystianKasp98 <krystiankasprzak98byd@gmail.com>
2023-06-13 16:53:59 +0200

    refactor

---
+++ hello-world/src/cats/cats.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { Cat } from './entities/cat.entity';
+
+@Injectable()
+export class CatsService {
+  private readonly cats: Cat[] = [];
+  create(cat: CreateCatDto) {
+    this.cats.push({ ...cat, age: 0 });
+  }
+
+  findAll(): Cat[] {
+    return this.cats;
+  }
+}
