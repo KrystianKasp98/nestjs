fbc943b7d7123624070a6ca898cfe54392e491ab
KrystianKasp98 <krystiankasprzak98byd@gmail.com>
2023-06-22 23:51:54 +0200

    add id to cat entity

--- hello-world/src/cats/cats.service.ts
+++ hello-world/src/cats/cats.service.ts
@@ -1,3 +1,4 @@
+import { process } from 'uniqid';
 import { Injectable } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { Cat } from './entities/cat.entity';
@@ -7,7 +8,7 @@ export class CatsService {
   private readonly cats: Cat[] = [];
 
   create(cat: CreateCatDto) {
-    const newCat = { ...cat, age: 0 };
+    const newCat = { ...cat, age: 0, id: process() };
     this.cats.push(newCat);
 
     return newCat;
